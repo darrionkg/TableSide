44b0461550d128473a8a6f2ea8901881d7cb3051
Kerri Walker <kerriannwalker3@gmail.com>
2019-06-24 13:43:24 -0700

    add html for list of parties

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
+  template: `<img src="assets/img/plus.png">`,
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
