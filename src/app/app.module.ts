30b2874c385a713851b2b24db3a267f5cd2e8640
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-24 14:51:33 -0700

    add parties to client constructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { ClientComponent } from './client/client.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { firebaseConfig } from 'api_keys';
+import { firebaseConfig } from './api_keys';
+//import { DashboardComponent } from './dashboard/dashboard.component';
 import { PartyDetailComponent } from './party-detail/party-detail.component';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
 import { MenuComponent } from './menu/menu.component';
@@ -17,6 +18,7 @@ import { MenuComponent } from './menu/menu.component';
   declarations: [
     AppComponent,
     ClientComponent,
+    //DashboardComponent,
     PartyDetailComponent,
     OrderDetailComponent,
     MenuComponent
--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, Location } from './../../services/data.service';
+import { Router } from '@angular/router';
+// import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-client',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
-  constructor() { }
+  parties: [] = [];
+  constructor(private dataService: DataService) {
+    // this.parties = dataService.
+   }
+  
+   addParty() {
+     this.dataService.addParty().then(ref => console.log(ref.id));
+   }
 
   ngOnInit() {
+    console.log();
   }
 
 }
+
