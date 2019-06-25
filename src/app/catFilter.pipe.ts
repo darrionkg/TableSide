81573931822f7ac298ada3fd4c4f2d99c0e7aee8
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-25 00:17:57 -0700

    build pipe to filter menu items according to category

---
+++ src/app/catFilter.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+// filter observable array by category property
+
+@Pipe({name: 'catFilter'})
+export  class CatFilter implements PipeTransform {
+  transform(catArray: Array<any>, category: string): Array<any> {
+    return catArray.filter(item => item.category === category);
+  }
+}
