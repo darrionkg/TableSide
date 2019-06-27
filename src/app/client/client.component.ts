8b4acb4fdd10722405c56b3ef4cb6205ee8d562d
Student <example@example.com>
2019-06-27 14:16:54 -0700

    remove wait function

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -33,12 +33,13 @@ export class ClientComponent implements OnInit, OnDestroy {
     });
   }
   updateWaitTime() {
-    if (this.parties && this.parties.length > 0) {
-      for (let i = 0; i < this.parties.length; i++) {
-        this.parties[i].waitTime =
-          Math.floor((Date.now() * .001 - this.parties[i]['timeSeated']['seconds']) / 60)
-      }
-    }
+    // if (this.parties && this.parties.length > 0) {
+    //   for (let i = 0; i < this.parties.length; i++) {
+    //     this.parties[i].waitTime = (this.parties[i].hasOwnProperty('timeSeated')) ?
+    //       Math.floor((Date.now() * .001 - this.parties[i]['timeSeated']['seconds']) / 60) :
+    //       0;
+    //   }
+    // }
   }
 
   ngOnInit() {
