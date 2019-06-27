823d07636916309329f4896245d2b6b1f8ce98e3
Student <example@example.com>
2019-06-27 12:59:25 -0700

    sort by status status bar

--- src/app/status-bar/status-bar.component.ts
+++ src/app/status-bar/status-bar.component.ts
@@ -7,14 +7,19 @@ import { DataService } from 'src/services/data.service';
   styleUrls: ['./status-bar.component.css']
 })
 export class StatusBarComponent implements OnInit {
-@Input() order: any;
-
-  items: any[];
+  @Input() order: any;
+  itemsStaged = [];
+  itemsSent = [];
+  itemsExpo = [];
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit() {
-    this.dataService.getOrderItems(this.order.id).subscribe(item => this.items = item);
+    this.dataService.getOrderItems(this.order.id).subscribe(item => {
+      this.itemsStaged = item.filter(ref => ref.status === 'staged');
+      this.itemsSent = item.filter(ref => ref.status === 'sent');
+      this.itemsExpo = item.filter(ref => ref.status === 'expo');
+    });
   }
 }
